ba6ca97c43c7244f82870a74c054297edb0d0663
Ali Saheli <ali@stuffak.com>
2012-12-27 21:45:22 -0800

    simple loading widget from js

---
+++ simple/widget.js
@@ -0,0 +1,10 @@
+document.write(
+    '   <div style="block">' +
+    '       <h4>Profile is loaded from <i>widget.js</i></h4>'+
+    '       <div style="float:left;margin-right:3px;">'+
+    '           <img src="http://graph.facebook.com/saheli.ali/picture"/>'+
+    '       </div>'+
+    '       <span><strong>Name:</strong> Ali S</strong></span><br/>' +
+    '       <span><strong>GitHub:</strong> https://github.com/apooboy/</span>' +
+    '   </div>'
+);
