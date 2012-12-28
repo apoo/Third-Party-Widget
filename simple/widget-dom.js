dea747800f606bdcb0027fa7ccfd96d9aa734d98
Ali Saheli <ali@stuffak.com>
2012-12-27 21:53:07 -0800

    new js , using document.create

--- simple/widget-dom.js
+++ simple/widget-dom.js
@@ -1,10 +1,16 @@
-document.write(
+var html =
     '   <div style="block">' +
-    '       <h4>Using JS <i>document.write</i></h4>'+
+    '       <h4>Using JS <i>create.element</i></h4>'+
     '       <div style="float:left;margin-right:3px;">'+
     '           <img src="http://graph.facebook.com/saheli.ali/picture"/>'+
     '       </div>'+
     '       <span><strong>Name:</strong> Ali S</strong></span><br/>' +
     '       <span><strong>GitHub:</strong> https://github.com/apooboy/</span>' +
-    '   </div>'
-);
+    '   </div>';
+
+var div = document.createElement('div');
+div.innerHTML = html;
+
+var appendTo = document.getElementById("widget-dom");
+appendTo.parentNode.insertBefore(div, appendTo);
+
